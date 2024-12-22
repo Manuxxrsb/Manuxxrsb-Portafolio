c44e66e241250669e0956642052a8858139ba2ff
Manuxxrsb <manuxxrsb@gmail.com>
2024-12-22 17:39:40 -0300

    Actualizar la URL de la imagen en el componente GitHubProjects para utilizar el enlace directo de raw.githubusercontent.com

--- src/components/Github-proyects.tsx
+++ src/components/Github-proyects.tsx
@@ -25,7 +25,7 @@ const GitHubProjects: React.FC = () => {
             className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
           >
             <img
-              src={`https://github.com/Manuxxrsb/${repo.name}/blob/main/public/image.png`}
+              src={`https://raw.githubusercontent.com/manuxxrsb/${repo.name}/main/image.png`}
               alt={`${repo.name}`}
             />
 
