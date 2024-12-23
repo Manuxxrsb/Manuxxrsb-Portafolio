69f6110e0806dead4d9054fbb8d71826dd37f6f0
Manuxxrsb <manuxxrsb@gmail.com>
2024-12-23 10:49:12 -0300

    Mejorar la presentación del componente Navbar añadiendo íconos y ajustando el espaciado

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,41 @@
 import { HouseIcon } from "lucide-react";
+import { GrProjects } from "react-icons/gr";
+import { IoPersonOutline } from "react-icons/io5";
+import { LiaToolsSolid } from "react-icons/lia";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar bg-white p-4 shadow-lg fixed top-0 left-0 w-full z-10">
+      <nav className="navbar bg-white p-4 shadow-md fixed top-0 left-0 w-full z-10">
         <div className="container mx-auto flex justify-between items-center">
           <h1 className="text-black text-2xl font-bold">
             Manuxxrsb Portafolio
           </h1>
-          <ul className="flex space-x-4">
-            <li className="m-4 p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300">
-              <Link to="/">
-                <HouseIcon size={24} />
+          <ul className="flex space-x-4 items-center space-between">
+            <li className="p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300 flex flex-row items-center">
+              <HouseIcon size={24} />
+              <Link to="/" className="ml-1">
+                Home
               </Link>
             </li>
-            <li className="m-4 p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300">
-              <Link to="/projects">Proyectos</Link>
+            <li className="m-4 p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300 flex flex-row items-center">
+              <GrProjects size={24} />
+              <Link to="/projects" className="ml-1.5">
+                Proyectos
+              </Link>
             </li>
-            <li className="m-4 p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300">
-              <Link to="/aboutme">Sobre mi</Link>
+            <li className="m-4 p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300 flex flex-row items-center">
+              <IoPersonOutline size={24} />
+              <Link to="/aboutme" className="ml-1">
+                Sobre mi
+              </Link>
             </li>
-            <li className="m-4 p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300">
-              <Link to="/barra_tecnologias">Conoce mis tecnologías</Link>
+            <li className="m-4 p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300 flex flex-row items-center">
+              <LiaToolsSolid />
+              <Link to="/barra_tecnologias" className="ml-1">
+                Tecnologías
+              </Link>
             </li>
           </ul>
         </div>
