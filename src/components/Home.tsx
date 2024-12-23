4e731a42ebd92c821bfce7daafdce82d2718114a
Manuxxrsb <manuxxrsb@gmail.com>
2024-12-23 20:09:06 -0300

    Agregar nuevos íconos en el componente Home para mejorar la presentación visual

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { AiOutlineBranches, AiOutlineLoading3Quarters } from "react-icons/ai";
+import {
+  AiOutlineBranches,
+  AiOutlineFolderOpen,
+  AiOutlineLaptop,
+  AiOutlineLoading3Quarters,
+  AiTwotoneBug,
+} from "react-icons/ai";
+import { VscCoffee } from "react-icons/vsc";
 
 const Home: React.FC = () => {
   return (
@@ -9,6 +16,22 @@ const Home: React.FC = () => {
         <h1 className="text-8xl font-bold">Software Engineer</h1>
         <h1 className="text-8xl font-bold text-orange-500">Manuel Solis</h1>
         <h2 className="text-4xl"></h2>
+        <VscCoffee
+          size={90}
+          className="absolute mt-32 left-10 text-orange-500"
+        />
+        <AiTwotoneBug
+          size={90}
+          className="absolute mt-32 right-10 text-orange-500"
+        />
+        <AiOutlineLaptop
+          size={90}
+          className="absolute mt-80 right-60 text-black"
+        />
+        <AiOutlineFolderOpen
+          size={90}
+          className="absolute mt-80 left-60 text-black"
+        />
         <div className="flex justify-center items-center mt-28">
           <AiOutlineLoading3Quarters
             size={70}
