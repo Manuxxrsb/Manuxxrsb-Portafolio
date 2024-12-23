b01df1cd161da09a64e669894367fbd836119d7a
Manuxxrsb <manuxxrsb@gmail.com>
2024-12-23 12:15:08 -0300

    Agregar menú desplegable en la barra de navegación para mejorar la usabilidad en dispositivos móviles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,15 @@ import { GrProjects } from "react-icons/gr";
 import { IoPersonOutline } from "react-icons/io5";
 import { LiaToolsSolid } from "react-icons/lia";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <nav className="navbar bg-white p-4 shadow-md fixed top-0 left-0 w-full z-10">
@@ -12,33 +19,33 @@ const Navbar = () => {
           <h1 className="text-black text-2xl font-bold">
             Manuxxrsb Portafolio
           </h1>
-          <ul className="flex space-x-4 items-center space-between">
-            <li className="p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300 flex flex-row items-center cursor-pointer">
-
+          <button className="block md:hidden text-black" onClick={toggleMenu}>
+            ☰
+          </button>
+          <ul
+            className={`flex-col md:flex-row md:flex space-x-4 items-start md:items-center space-between ${
+              isOpen ? "flex" : "hidden"
+            } md:space-y-0 space-y-4 md:space-x-4`}
+          >
+            <li className="m-4 p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300 flex flex-row items-center cursor-pointer">
               <HouseIcon size={24} />
               <Link to="/" className="ml-1">
                 Home
               </Link>
             </li>
-
             <li className="m-4 p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300 flex flex-row items-center cursor-pointer">
-
               <GrProjects size={24} />
               <Link to="/projects" className="ml-1.5">
                 Proyectos
               </Link>
             </li>
-
             <li className="m-4 p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300 flex flex-row items-center cursor-pointer">
-
               <IoPersonOutline size={24} />
               <Link to="/aboutme" className="ml-1">
                 Sobre mi
               </Link>
             </li>
-
             <li className="m-4 p-2 bg-orange-500 text-white rounded-md shadow-md hover:bg-orange-600 transition duration-300 flex flex-row items-center cursor-pointer">
-
               <LiaToolsSolid />
               <Link to="/barra_tecnologias" className="ml-1">
                 Tecnologías
