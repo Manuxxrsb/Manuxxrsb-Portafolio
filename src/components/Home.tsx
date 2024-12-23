2cefaf624f07a8ebe93e2d03910018e87dbfe682
Manuxxrsb <manuxxrsb@gmail.com>
2024-12-23 10:47:00 -0300

    Actualizar estilos en GitHubProjects y cambiar título en Home a "Software Engineer"

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center mt-36">
-      <h1 className="text-8xl font-bold">Computer Engineer</h1>
+      <h1 className="text-8xl font-bold">Software Engineer</h1>
       <h1 className="text-8xl font-bold text-orange-500">Manuel Solis</h1>
       <h2 className="text-4xl"></h2>
     </div>
